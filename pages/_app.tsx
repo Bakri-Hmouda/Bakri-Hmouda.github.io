8ab8d9fa6fa95dc5ca3c97b848bafc0955676ae6
Bakrifly <bakri.hmouda@gmail.com>
2022-11-28 23:28:18 +0200

    added splash screen on production while site being built

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,19 @@
-import { AppProps} from "next/app";
+import { AppProps } from "next/app";
 
-export default function App({Component,pageProps}:AppProps){
-    return <Component {...pageProps}/>
+
+function check_if_in_Development() {
+    const env = process.env.NODE_ENV
+    console.log(env)
+    if (env == "development") {
+        return true
+    }
+    else if (env == "production") {
+        return false
+    }
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+    //   display build message in production
+    if (!check_if_in_Development()) { return <h1>website is being built!</h1> }
+    return <Component {...pageProps} />
 }
