dba9004986b59ff808d875ef95198b820f8b5ac4
Bakrifly <bakri.hmouda@gmail.com>
2022-11-28 23:05:30 +0200

    added file after git ignore fix

---
+++ pages/welcome/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import styles from "../../styles/Welcome.module.css"
+
+export default function Welcome() {
+
+    return (
+        <>
+            <Head>
+                <title>Welcome | Bakri Hmouda</title>
+            </Head>
+
+            <main>
+                <div className={styles.container}>
+
+                </div>
+            </main>
+        </>
+    )
+
+}
